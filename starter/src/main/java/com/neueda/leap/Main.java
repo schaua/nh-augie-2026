c534362f9954eddfd646950f386358b2944b6add
August Schau <schaua@augustschau.com>
2026-08-25 10:57:45 -0400

    feat:Added augie data

--- starter/src/main/java/com/neueda/leap/Main.java
+++ starter/src/main/java/com/neueda/leap/Main.java
@@ -3,5 +3,7 @@ package com.neueda.leap;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from the team's Sprint 1 project skeleton");
-    }
+        System.out.println("This is a demo for pull requests on GitHub.");
+        System.out.println("Augie is a artist");    
+        }
 }
