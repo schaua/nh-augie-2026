c9cdd8d6caf79736e5134767fd43ec809a160062
August Schau <schaua@augustschau.com>
2026-08-30 11:08:53 -0400

    demo:Create sample database for joins

--- starter/src/main/java/com/neueda/leap/Main.java
+++
@@ -1,9 +0,0 @@
-package com.neueda.leap;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world from the team's Sprint 1 project skeleton");
-        System.out.println("This is a demo for pull requests on GitHub.");
-        System.out.println("Fred is a dancer");
-    }
-}
